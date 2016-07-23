bd3b766baf046697cbdc3d4174da2f97d9b35e92
Siddhesh <siddgh.sd@icloud.com>
2016-07-24 05:02:00 +0530

    Dyanamic Data on a Single Activity

--- Android/Atma_14/app/src/main/java/com/atma/atma_14/TopExpand.java
+++ Android/Atma_14/app/src/main/java/com/atma/atma_14/TopExpand.java
@@ -10,7 +10,7 @@ public class TopExpand extends AppCompatActivity {
 
     String TopType;
     CircleImageView currentImage,currentProject1,currentProject2,currentProject3,currentProject4;
-    TextView currentName,currentProjectTitle1,currentProjectTitle2,currentProjectTitle3,currentProjectTitle4;
+    TextView currentSummary,currentName,currentProjectTitle1,currentProjectTitle2,currentProjectTitle3,currentProjectTitle4,TopTypeIndicator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +18,77 @@ public class TopExpand extends AppCompatActivity {
         TopType = getIntent().getStringExtra("TopType");
         setUpViews();
         switch (TopType){
+            case "Top Valunteer":
+                currentName.setText("Niveta Lavhate");
+                currentImage.setImageResource(R.drawable.girl);
+                currentSummary.setText("Volunteering with ATMA foundation was one of the best decisions I ever made. Not only did it provide me great exposure and hands on experience organising and managing events, it also brought about the much needed tact I lacked in handling certain relationships. Today looking back, I miss the pearls of wisdom that was freely given, the constant vibe of camaraderie, and the fresh and hot afternoon snacks! ATMA foundation will always be thought of with a feeling of immense gratitude");
 
+                currentProject1.setImageResource(R.drawable.ngo3);
+                currentProject2.setImageResource(R.drawable.ngo5);
+                currentProject3.setImageResource(R.drawable.ngo1);
+                currentProject4.setImageResource(R.drawable.ngo4);
+
+                currentProjectTitle1.setText("ACSI");
+                currentProjectTitle2.setText("Bread for the world");
+                currentProjectTitle3.setText("Mama's Children");
+                currentProjectTitle4.setText("CFSD");
+
+                TopTypeIndicator.setText("Top Volunteer");
+                break;
+            case "Top Partner Manager":
+
+                currentName.setText("Ronak Parmar");
+                currentImage.setImageResource(R.drawable.boy);
+                currentSummary.setText("Living in Harmony’ training programme has highly improved the happiness quotient of our employees. We realised that a happy family of every employee results in better productivity at work effortlessly and it inevitably results in higher customer satisfaction.");
+
+                currentProject1.setImageResource(R.drawable.ngo4);
+                currentProject2.setImageResource(R.drawable.ngo3);
+                currentProject3.setImageResource(R.drawable.ngo5);
+                currentProject4.setImageResource(R.drawable.ngo1);
+
+                currentProjectTitle1.setText("CFSD");
+                currentProjectTitle2.setText("ASCI");
+                currentProjectTitle3.setText("Bread for the world");
+                currentProjectTitle4.setText("Mama's Children");
+
+                TopTypeIndicator.setText("Top Partner Manager");
+                break;
+            case "Top NGO":
+
+                currentName.setText("Ronak Parmar");
+                currentImage.setImageResource(R.drawable.boy);
+                currentSummary.setText("Living in Harmony’ training programme has highly improved the happiness quotient of our employees. We realised that a happy family of every employee results in better productivity at work effortlessly and it inevitably results in higher customer satisfaction.");
+
+                currentProject1.setImageResource(R.drawable.ngo4);
+                currentProject2.setImageResource(R.drawable.ngo3);
+                currentProject3.setImageResource(R.drawable.ngo5);
+                currentProject4.setImageResource(R.drawable.ngo1);
+
+                currentProjectTitle1.setText("CFSD");
+                currentProjectTitle2.setText("ASCI");
+                currentProjectTitle3.setText("Bread for the world");
+                currentProjectTitle4.setText("Mama's Children");
+
+                TopTypeIndicator.setText("Top Partner Manager");
+
+                break;
         }
     }
 
     private void setUpViews() {
         currentImage = (CircleImageView) findViewById(R.id.current_image);
-        currentProject1 = (CircleImageView) findViewById(R.id.)
+        currentProject1 = (CircleImageView) findViewById(R.id.top_expand_currentworking1_image);
+        currentProject2 = (CircleImageView) findViewById(R.id.top_expand_currentworking2_image);
+        currentProject3 = (CircleImageView) findViewById(R.id.top_expand_currentworking3_image);
+        currentProject4 = (CircleImageView) findViewById(R.id.top_expand_currentworking4_image);
+
+        currentName = (TextView) findViewById(R.id.current_name);
+        currentSummary = (TextView) findViewById(R.id.current_summary);
+        currentProjectTitle1 = (TextView) findViewById(R.id.top_expand_currentworking1_textview);
+        currentProjectTitle2 = (TextView) findViewById(R.id.top_expand_currentworking2_textview);
+        currentProjectTitle3 = (TextView) findViewById(R.id.top_expand_currentworking3_textview);
+        currentProjectTitle4 = (TextView) findViewById(R.id.top_expand_currentworking4_textview);
+
+        TopTypeIndicator = (TextView) findViewById(R.id.toptypeIndicator);
     }
 }
