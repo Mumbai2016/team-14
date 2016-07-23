4a1721325f38e292d5aa85b14429979ba8c1b27c
KrutantaK <kt.katnaturk@gmail.com>
2016-07-23 23:44:43 +0530

    dashboard updated

--- html/login.php
+++ html/login.php
@@ -2,7 +2,7 @@
 <html >
   <head>
     <meta charset="UTF-8">
-    <title>login form</title>    
+    <title>Atma</title>    
     <link rel="stylesheet" href="../css/reset.css">
     <style>
       * {
