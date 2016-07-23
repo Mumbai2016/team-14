403f67e465361ed30fdb3d0e37e5cc4ffb76b528
KrutantaK <kt.katnaturk@gmail.com>
2016-07-24 02:06:47 +0530

    Volunteer details updated dynamically

--- html/volunteer_details.php
+++ html/volunteer_details.php
@@ -1,157 +1,120 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
+    <meta charset="utf-8" />
+    <title>Hover User Details Template | PrepBootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap-3.3.6-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/volunteer_details.css" />
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <!--link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" /-->
 
-    <title>Round About - Start Bootstrap Template</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/startbootstrap-round-about-1.0.4/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../css/startbootstrap-round-about-1.0.4/css/round-about.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-	<style>
-		img{
-			width: 250px;
-			height: 250px;
-		}
-		
-	</style>
+    <script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
 </head>
-
 <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Atma</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Register</a>
-                    </li>
-                    <li>
-                        <a href="#">Login</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
+<div class="main-container">
     <div class="container">
-
-        <!-- Introduction Row -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Atma
-                    <small>Quality education for all children.</small>
-                </h1>
-                <p>Building capacity of education NGOs to achieve sustainability and scalability thereby enabling delivery of quality education.<br>We provide partners with a systematic approach to grow and amplify their impact. The way we work is crucial to our success. We combine a strong result focused approach with a collaborative attitude. We don't just 'pass by' as consultants, but really walk our talk together with our partners.</p>
-            </div>
-        </div>
-
-        <!-- Team Members Row -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">Our Top Performers</h2>
-            </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="../img/girl1.jpe" alt="">
-                <h3>Niveta Lavathe
-                    <small>Volunteer</small>
-                </h3>
-                <p>A special thanks to <a target="_blank" href="#">Niveta Lavathe</a> for providing her wonderful services and being the top volunteer of the quarter.</p>
-            </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
-                <h3>Dream A Dream
-                    <small>NGO</small>
-                </h3>
-                <p>Best performer NGO of the Quarter!</p>
-            </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            </div>
-		</div>
-		<div class="row">
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-            </div>
-        </div>
-
-        <hr>
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
-
+    <div class="page-header">
+        <h1><center>VOLUNTEER</center></h1>
     </div>
-    <!-- /.container -->
+    <div class="container">
+        
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="form_hover" style="height:400px;">
+                    <p style="text-align: center; margin-top: 20px;">
+                        <img src="../img/girl1.jpe" alt="">
+                    </p>
+                    <div class="header">
+                        <div class="blur"></div>
+                        <div class="header-text" >
+                            <div class="panel panel-success" style="height: 400px;margin-top:150px;">
+                                <div class="panel-heading">
+                                    <h3 style="color: #428BCA;">User Details</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        Last login Date:<b>02/09/2013</b>
+                                    </div>
+                                    <div class="form-group">
+                                        Download Count:<b>104</b>
+                                    </div>
+                                    <div class="form-group">
+                                        User Rating:<br />
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star-o"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+</div>
+<style>
+    .form_hover {
+        padding: 0px;
+        position: relative;
+        overflow: hidden;
+        height: 240px;
+    }
 
-    <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
+        .form_hover:hover .header {
+            opacity: 1;
+            transform: translateY(-172px);
+            -webkit-transform: translateY(-172px);
+            -moz-transform: translateY(-172px);
+            -ms-transform: translateY(-172px);
+            -o-transform: translateY(-172px);
+        }
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
+        .form_hover img {
+            z-index: 4;
+        }
+
+        .form_hover .header {
+            position: absolute;
+            top: 170px;
+            -webkit-transition: all 0.3s ease;
+            -moz-transition: all 0.3s ease;
+            -o-transition: all 0.3s ease;
+            -ms-transition: all 0.3s ease;
+            transition: all 0.3s ease;
+            width: 100%;
+        }
+
+        .form_hover .blur {
+            height: 240px;
+            z-index: 5;
+            position: absolute;
+            width: 100%;
+        }
+
+        .form_hover .caption-text {
+            z-index: 10;
+            color: #fff;
+            position: absolute;
+            height: 240px;
+            text-align: center;
+            top: -20px;
+            width: 100%;
+        }
+</style>
+
+
+<!-- Hover User Details - END -->
+
+</div>
 
 </body>
-
 </html>
