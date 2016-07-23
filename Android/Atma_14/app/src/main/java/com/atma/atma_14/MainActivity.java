f4212058d4db8de60141fda360e786af89d54d16
Siddhesh <siddgh.sd@icloud.com>
2016-07-23 18:56:40 +0530

    Login User Type Updated
    Login User type Ui and Logic Implemented and working

--- Android/Atma_14/app/src/main/java/com/atma/atma_14/MainActivity.java
+++ Android/Atma_14/app/src/main/java/com/atma/atma_14/MainActivity.java
@@ -1,13 +1,51 @@
 package com.atma.atma_14;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    RadioGroup userTypeGroup;
+    RadioButton radioVolunteer,radioNGO,radioProjectManager;
+    FloatingActionButton fabNext;
+    Toolbar toolbar;
+    String selectedLoginType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.login_as_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupViews();
+    }
+
+    public void nextClick(View v){
+        int checkRadio = userTypeGroup.getCheckedRadioButtonId();
+        switch (checkRadio){
+            case R.id.login_as_radiobutton_ngo:
+                selectedLoginType = "NGO";
+                break;
+            case R.id.login_as_radiobutton_partnership_manager:
+                selectedLoginType = "Partnership Manager";
+                break;
+            case R.id.login_as_radiobutton_volunteer:
+                selectedLoginType = "Volunteer";
+                break;
+        }
+    }
+
+    private void setupViews() {
+        userTypeGroup = (RadioGroup) findViewById(R.id.login_as_radiogroup);
+        radioVolunteer = (RadioButton) findViewById(R.id.login_as_radiobutton_volunteer);
+        radioNGO = (RadioButton) findViewById(R.id.login_as_radiobutton_ngo);
+        radioProjectManager = (RadioButton) findViewById(R.id.login_as_radiobutton_partnership_manager);
+        fabNext = (FloatingActionButton) findViewById(R.id.login_as_nextfab);
     }
 }
