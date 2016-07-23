b05668c9e87f973144845c7988ac37e1994d2189
Siddhesh <siddgh.sd@icloud.com>
2016-07-24 03:43:32 +0530

    NGO Acitivity Completed

--- Android/Atma_14/app/src/main/java/com/atma/atma_14/NGOActivity.java
+++ Android/Atma_14/app/src/main/java/com/atma/atma_14/NGOActivity.java
@@ -2,23 +2,29 @@ package com.atma.atma_14;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class NGOActivity extends AppCompatActivity {
+public class NGOActivity extends AppCompatActivity implements View.OnClickListener{
 
     LinearLayout ProjectClick1,ProjectClick2,ProjectClick3,ProjectClick4;
     CircleImageView ProjectImage1,ProjectImage2,ProjectImage3,ProjectImage4;
-    TextView ProjectTitle1,ProjectTitle2,ProjectTitle3,ProjectTitle4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ngo);
         setUpViews();
+
+        ProjectClick1.setOnClickListener(this);
+        ProjectClick2.setOnClickListener(this);
+        ProjectClick3.setOnClickListener(this);
+        ProjectClick4.setOnClickListener(this);
+
     }
+
 
     private void setUpViews() {
         ProjectClick1 = (LinearLayout) findViewById(R.id.NGOLayout1);
@@ -30,11 +36,21 @@ public class NGOActivity extends AppCompatActivity {
         ProjectImage2 = (CircleImageView) findViewById(R.id.NGOImage2);
         ProjectImage3 = (CircleImageView) findViewById(R.id.NGOImage3);
         ProjectImage4 = (CircleImageView) findViewById(R.id.NGOImage4);
+    }
 
-        ProjectTitle1 = (TextView) findViewById(R.id.NGOProjectName1);
-        ProjectTitle2 = (TextView) findViewById(R.id.NGOProjectName2);
-        ProjectTitle3 = (TextView) findViewById(R.id.NGOProjectName3);
-        ProjectTitle4 = (TextView) findViewById(R.id.NGOProjectName4);
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.NGOLayout1:
+                break;
+            case R.id.NGOLayout2:
+                break;
+            case R.id.NGOLayout3:
+                break;
+            case R.id.NGOLayout4:
+                break;
+        }
     }
 }
 
