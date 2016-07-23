a4478655c7d1c41d893f634967b9928e4a4846ab
mahi96 <parmarmahendra96@yahoo.in>
2016-07-23 23:25:01 +0530

    volunteer screening/ selection created.

---
+++ html/volunteer.php
@@ -0,0 +1,31 @@
+<html>
+<HEAD>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</HEAD>
+<body>
+	<div class="container">
+    <h2>Volunteer Details</h2>
+    <div class="table-responsive">
+	<table class="table">
+	<tr>
+	<th>Volunteer name</th>
+	<th>Gender</th>
+	<th>Email</th>
+	<th>Motivation</th>
+	<th>Resume</th>
+	<th>Screening</th>
+	<th>Selection</th></tr>
+	<tr>
+	<td>dsdss</td>
+	<td>MAle</td>
+	<td>dsdss@gmail.com</td>
+	<td>Motivation</td>
+	<td>sddsdsd.pdf</td>
+	<td><input type="button" name="select" value="SCREEN"></td>
+	<td><input type="button" name="select" value="SELECT"></td></tr>
+	</table>
+	</div>
+	</div>
+
+</body>
+</html>
