c63374caecc70ccc15cc8cb8853b77acfe06a796
KrutantaK <kt.katnaturk@gmail.com>
2016-07-24 00:47:06 +0530

    volunteer updated

--- html/volunteer.php
+++ html/volunteer.php
@@ -1,31 +1,119 @@
 <html>
-<HEAD>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-</HEAD>
+<head>
+    <meta http-equiv="content-type" content="text/php; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+    <title>Atma</title>    
+    <link rel="stylesheet" href="../css/volunteer.css" />
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="../js/volunteer.js" type="text/javascript"></script>
+    </script>
+</head>
 <body>
-	<div class="container">
-    <h2>Volunteer Details</h2>
-    <div class="table-responsive">
-	<table class="table">
-	<tr>
-	<th>Volunteer name</th>
-	<th>Gender</th>
-	<th>Email</th>
-	<th>Motivation</th>
-	<th>Resume</th>
-	<th>Screening</th>
-	<th>Selection</th></tr>
-	<tr>
-	<td>dsdss</td>
-	<td>MAle</td>
-	<td>dsdss@gmail.com</td>
-	<td>Motivation</td>
-	<td>sddsdsd.pdf</td>
-	<td><input type="button" name="select" value="SCREEN"></td>
-	<td><input type="button" name="select" value="SELECT"></td></tr>
-	</table>
-	</div>
-	</div>
+    <div>
+    <nav class="main-menu">
+        <ul>
+            <li>
+                <i class="fa fa-home fa-2x"></i>
+                <span class="nav-text">
+                    Account
+                </span>
+            
+            </li>
+            <li>
+                <a href ="#" onclick="load()"   >
+                    <i class="fa fa-home fa-2x"></i>
+                    <span class="nav-text">
+                        Dashboard
+                    </span>
+                </a>
+              
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                    <i class="fa fa-laptop fa-2x"></i>
+                    <span class="nav-text">
+                        UI Components
+                    </span>
+                </a>
+                
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                   <i class="fa fa-list fa-2x"></i>
+                    <span class="nav-text">
+                        Forms
+                    </span>
+                </a>
+                
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                   <i class="fa fa-folder-open fa-2x"></i>
+                    <span class="nav-text">
+                        Pages
+                    </span>
+                </a>
+               
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-bar-chart-o fa-2x"></i>
+                    <span class="nav-text">
+                        Graphs and Statistics
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-font fa-2x"></i>
+                    <span class="nav-text">
+                        Typography and Icons
+                    </span>
+                </a>
+            </li>
+            <li>
+               <a href="#">
+                   <i class="fa fa-table fa-2x"></i>
+                    <span class="nav-text">
+                        Tables
+                    </span>
+                </a>
+            </li>
+            <li>
+               <a href="#">
+                    <i class="fa fa-map-marker fa-2x"></i>
+                    <span class="nav-text">
+                        Maps
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                   <i class="fa fa-info fa-2x"></i>
+                    <span class="nav-text">
+                        Documentation
+                    </span>
+                </a>
+            </li>
+        </ul>
+
+        <ul class="logout">
+            <li>
+               <a href="#">
+                     <i class="fa fa-power-off fa-2x"></i>
+                    <span class="nav-text">
+                        Logout
+                    </span>
+                </a>
+            </li>  
+        </ul>
+    </nav>
+
+    <div id="area">
+    </div>
+    </div>
 
 </body>
 </html>
--- js/project_manager.js
+++ js/project_manager.js
@@ -1,3 +1,3 @@
 function load(){
-		document.getElementById("area").innerHTML='<object type="text/php" data="dashboard1.php" style="height:100%;width:100%;" ></object>';
+		document.getElementById("area").innerHTML='<object type="text/php" data="mini_dashboard.php" style="height:100%;width:100%;" ></object>';
 	}
