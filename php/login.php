7ffd1a1086e03131cdef78179e0ba0fe365c39f4
mahi96 <parmarmahendra96@yahoo.in>
2016-07-24 06:31:19 +0530

    login page changes for aws server

--- php/login.php
+++ php/login.php
@@ -4,7 +4,7 @@ $username=$_POST['username'];
 $password=$_POST['password'];
 
 
-$conn = mysqli_connect("localhost","root","","atma");
+$conn = mysqli_connect("localhost","root","root","atma");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
